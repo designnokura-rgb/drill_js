de7db13d61e335469cd217f144ea50fc22bf5c2f
Yusuke Fukumoto <ysk2645j4@gmail.com>
2026-01-29 23:59:41 +0900

    start/js/script.jsのコメントをfinish/js/script.jsに合わせて更新

--- start/js/script.js
+++ start/js/script.js
@@ -18,14 +18,14 @@
 //HTML Q3 の ul要素の子要素へ　liを3つ追加
 //liには「卵」「砂糖」「醤油」と表示させてください
 
-const material = ["卵","砂糖","醤油"];
+const material = ["卵", "砂糖", "醤油"];
 
 
 // Q4.
-//categoryが"トップス"の商品だけを抜き出して
+//categoryが"イス"の商品だけを抜き出して
 //nameを ulの中に li として表示させてください
 
-const  chairs= [
+const chairs = [
     { id: 1, name: "LC2", category: "イス", price: 605000 },
     { id: 2, name: "マシュマロソファ", category: "ソファ", price: 1199000 },
     { id: 3, name: "ラ・シェーズ", category: "イス", price: 1161600 },
@@ -36,7 +36,7 @@ const  chairs= [
 
 //Q5.
 // スコアが80点以上の生徒だけ抜き出して
-// HTML 05 のclass="over"内にカンマ区切りで表示させてください
+// HTML 05 のclass="over"内にリストで表示させてください
 const students = [
     { name: "早川", score: 85 },
     { name: "吉田", score: 72 },
@@ -57,7 +57,7 @@ const Jsweets = [
 
 
 // Q7.
-//2倍した結果を HTMLのclass="double-result" に表示する関数を作成してください
+//2倍した結果を HTMLのclass="double-result" に表示するshowDouble関数を作成してください
 //作った関数には25を渡してください
 
 
@@ -70,24 +70,28 @@ const Jsweets = [
 
 // Q9.
 // 生年月日（文字列）birthdayを引数として受け取り現在の年齢を算出してください
-// 帰ってきた年齢を class="age"に表示してください
+// 返ってきた年齢を class="age"に表示してください
 
 //Dateが認識できる書き方
 const birthday = "1995-05-16";
-        
+
 
 
 
 // Q10.
-// チェックボックスをクリックしたとき
-// weight が500未満の生き物だけを
-// ul に li として表示させてください
-// チェックが外れた場合は、すべての生き物を表示してください
+/*
+1. animals配列を使って、全ての動物を<ul class="animal-list"></ul>内にリストで表示するrenderAnimals関数を定義してください。
+引数: animals
+戻り値: なし
+※表示例はHTMLファイルのコメントアウトを参照
+
+2. チェックボックスにチェックが入っている場合、weight が500未満の生き物だけが表示されるようにしてください。チェックが外れたら、再度全ての生き物が表示されるようにしてください
+*/
 
 const animals = [
-    { name: "シャチ", weight: 3000 ,live: "知床半島"},
-    { name: "イルカ", weight: 150 ,live: "熊本天草"},
-    { name: "クジラ", weight: 25000 ,live: "小笠原諸島"},
-    { name: "スナメリ", weight: 40 ,live: "瀬戸内海"}
+    { name: "シャチ", weight: 3000, live: "知床半島" },
+    { name: "イルカ", weight: 150, live: "熊本天草" },
+    { name: "クジラ", weight: 25000, live: "小笠原諸島" },
+    { name: "スナメリ", weight: 40, live: "瀬戸内海" }
 ];
 
